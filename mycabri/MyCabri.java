82d4be65384b36c0a7b30dc4061ce84865206bbc
sroux <st.roux.gg@gmail.com>
2019-06-26 19:30:01 +0200

    Main du projet my cabri

---
+++ mycabri/MyCabri.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycabri;
+
+import controleur.*;
+
+/**
+ *
+ * @author Stéphane ROUX
+ */
+public class MyCabri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controleur c = new Controleur();
+
+    }
+}
